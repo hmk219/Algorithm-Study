4f5a184c6bf60263a59cafcd4e4b5045d2507f73
hmk219 <hmkreator219@gmail.com>
2021-09-08 00:49:30 +0900

    Bubble Sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
+
 //다음의 숫자들을 오름차순으로 정렬하는 프로그램을 작성하세요.
 //        1 10 5 8 7 6 4 3 2 9
 public class BubbleSort {
     public static void main(String[] args) {
+        int[] arr = {1, 10, 5, 8, 7, 6, 4, 3, 2, 9};
 
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<9-i; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
